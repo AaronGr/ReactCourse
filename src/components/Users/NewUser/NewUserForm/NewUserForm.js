7d547e6fba9caf100c8c94a77ddee35050cd0d99
Aaron Gravelle <agrav12825@gmail.com>
2022-08-11 16:18:00 -0500

    add styling to form

--- src/components/Users/NewUser/NewUserForm/NewUserForm.js
+++ src/components/Users/NewUser/NewUserForm/NewUserForm.js
@@ -30,11 +30,19 @@ const NewUserForm = props => {
 
     return (
         <form onSubmit={formSubmitHandler}>
-            <label>Username</label>
-            <input type="text" value={enteredUsername} onChange={usernameInputHandler} />
-            <label>Age(Years)</label>
-            <input type="number" value={enteredAge} onChange={ageInputHandler} min="1" step="1" />
-            <button type="submit">Add User</button>
+            <div className={styles.controls}>
+                <div className={styles.control}>
+                    <label><strong>Username</strong></label>
+                    <input type="text" value={enteredUsername} onChange={usernameInputHandler} />
+                </div>
+                <div className={styles.control}>
+                    <label><strong>Age(Years)</strong></label>
+                    <input type="number" value={enteredAge} onChange={ageInputHandler} min="1" step="1" />
+                </div>
+            </div>
+            <div className={styles.action}>
+                <button type="submit"><strong>Add User</strong></button>
+            </div>
         </form>
     )
 };
