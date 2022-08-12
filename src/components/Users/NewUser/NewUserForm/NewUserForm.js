b03c690e7d49870596d491a79904b6e7115fcabf
Aaron Gravelle <agrav12825@gmail.com>
2022-08-12 11:45:21 -0500

    replace state for inputs with refs

--- src/components/Users/NewUser/NewUserForm/NewUserForm.js
+++ src/components/Users/NewUser/NewUserForm/NewUserForm.js
@@ -1,37 +1,33 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import ErrorModal from '../../../UI/Toast/Toast';
 
 import styles from './NewUserForm.module.css';
 
 const NewUserForm = props => {
-    const [enteredUsername, setEnteredUsername] = useState('');
-    const [enteredAge, setEnteredAge] = useState('');
+    const usernameInputRef = useRef();
+    const ageInputRef = useRef();
+
     const [submissionIsInvalid, setSubmissionIsInvalid] = useState(false);
-
-    const usernameInputHandler = event => {
-        setEnteredUsername(event.target.value);
-        console.log(enteredUsername);
-    }
-
-    const ageInputHandler = event => {
-        setEnteredAge(event.target.value)
-    }
 
     const formSubmitHandler = event => {
         event.preventDefault();
+        const enteredName = usernameInputRef.current.value;
+        const enteredUserAge = ageInputRef.current.value;
+
         const newUser = {
-            username: enteredUsername,
-            age: +enteredAge,
+            username: enteredName,
+            age: +enteredUserAge,
             id: Math.random()
         }
         
-        if (!enteredUsername || !enteredAge) {
+        if (!enteredName || !enteredUserAge) {
             setSubmissionIsInvalid(true);
         } else {
             props.addNewUser(newUser);
         }
-        setEnteredUsername('');
-        setEnteredAge('');
+
+        usernameInputRef.current.value = '';
+        ageInputRef.current.value = '';
     }
 
     const toggleToast = () => {
@@ -47,15 +43,25 @@ const NewUserForm = props => {
                 <div className={styles.controls}>
                     <div className={styles.control}>
                         <label><strong>Username</strong></label>
-                        <input type="text" value={enteredUsername} onChange={usernameInputHandler} />
+                        <input 
+                            type="text" 
+                            ref={usernameInputRef} />
                     </div>
                     <div className={styles.control}>
                         <label><strong>Age(Years)</strong></label>
-                        <input type="number" value={enteredAge} onChange={ageInputHandler} min="1" step="1" />
+                        <input 
+                            type="number" 
+                            min="1" 
+                            step="1" 
+                            ref={ageInputRef}
+                            />
                     </div>
                 </div>
                 <div className={styles.action}>
-                    <button type="submit"><strong>Add User</strong></button>
+                    <button 
+                        type="submit">
+                            <strong>Add User</strong>
+                    </button>
                 </div>
             </form>
         </>
