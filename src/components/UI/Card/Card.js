73bfc09c30268e6675e8f5faf594dbad26b0bd88
Aaron Gravelle <agrav12825@gmail.com>
2022-08-12 11:55:00 -0500

    remove old project

--- src/components/UI/Card/Card.js
+++
@@ -1,9 +0,0 @@
-import styles from './Card.module.css'
-
-const Card = (props) => {
-    const classes = 'card ' + props.className;
-
-    return <div className={styles.card + ' ' + classes}>{props.children}</div>
-}
-
-export default Card;
