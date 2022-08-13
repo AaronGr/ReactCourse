ad46d6830e0de4708f4557e0d61eeb4387b94c56
Aaron Gravelle <agrav12825@gmail.com>
2022-08-13 12:29:38 -0500

    initial project

--- src/App.js
+++ src/App.js
@@ -1,21 +1,8 @@
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
-import { useContext } from 'react';
-import AuthContext from './components/store/auth-context';
-import React from 'react';
-
 function App() {
-  const ctx = useContext(AuthContext);
-
   return (
-    <React.Fragment>
-      <MainHeader />
-      <main>
-        {!ctx.isLoggedIn && <Login />}
-        {ctx.isLoggedIn && <Home />}
-      </main>
-    </React.Fragment>
+    <div>
+      <h2>Let's get started!</h2>
+    </div>
   );
 }
 
