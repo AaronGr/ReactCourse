b18011b8c0d340d2cbd0dec7a8f960fe6e9dccbf
Aaron Gravelle <agrav12825@gmail.com>
2022-08-18 20:40:46 -0500

    cart array in cart context

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -9,18 +9,6 @@ import CartItem from './CartItem/CartItem';
 import CartContext from '../../store/CartContext';
 
 const Cart = (props) => { 
-    const DUMMY_CART_DATA = [
-        {
-            id: 'm1',
-            name: 'Sushi',
-            price: 22.99
-        },
-        {
-            id: 'm3',
-            name: 'Schnitzel',
-            price: 16.50
-        }
-    ]
 
     const [totalPrice, setTotalPrice] = useState(0.0.toFixed(2));
 
@@ -29,20 +17,9 @@ const Cart = (props) => {
     const cartToggleHandler = () => {
         cartCtx.toggleCart();
     };
-
-    useEffect(() => {
-        let priceCount = 0;
-        let count = 0;
-        DUMMY_CART_DATA.forEach(item => {
-            priceCount += item.price;
-            count++;
-        });
-        setTotalPrice(priceCount.toFixed(2));
-        cartCtx.changeCartAmount(count);
-    }, [DUMMY_CART_DATA])
     
 
-    const meals = DUMMY_CART_DATA.map(item => {
+    const meals = cartCtx.cart.map(item => {
         return <CartItem key={item.id} meal={item} />
     });
 
--- src/store/CartContext.js
+++ src/store/CartContext.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 const CartContext = React.createContext({
     isCartOpen: false,
     cartAmount: 0,
+    cart: [],
     changeCartAmount: (amount) => {},
     toggleCart: () => {}
 });
@@ -10,6 +11,21 @@ const CartContext = React.createContext({
 export const CartContextProvider = props => {
     const [isCartOpen, setIsCartOpen] = useState(false);
     const [cartAmount, setCartAmount] = useState(0);
+    const [cart, setCart] = useState([]);
+
+
+    const DUMMY_CART_DATA = [
+        {
+            id: 'm1',
+            name: 'Sushi',
+            price: 22.99
+        },
+        {
+            id: 'm3',
+            name: 'Schnitzel',
+            price: 16.50
+        }
+    ]
 
     const changeCartAmountHandler = amount => {
         setCartAmount(amount);
@@ -24,6 +40,7 @@ export const CartContextProvider = props => {
             value={{
                 isCartOpen: isCartOpen,
                 cartAmount: cartAmount,
+                cart: DUMMY_CART_DATA,
                 changeCartAmount: changeCartAmountHandler,
                 toggleCart: toggleCartHandler
             }}
