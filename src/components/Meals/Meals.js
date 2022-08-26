bd7a7900035440eba1392f580dc2b2d4d51aa9bd
Aaron Gravelle <agrav12825@gmail.com>
2022-08-26 17:23:21 -0500

    add http hook and fetch menu from backend

--- src/components/Meals/Meals.js
+++ src/components/Meals/Meals.js
@@ -1,43 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from '../UI/Card/Card';
 import MealItem from './MealItem/MealItem';
+import useHttp from '../../hooks/use-http';
 
 import styles from './Meals.module.css';
 import MealsSummary from './MealsSummary/MealsSummary';
 
 const Meals = props => {
-    const menu = [
-        {
-            id: 'm1',
-            name: 'Sushi',
-            description: 'Finest fish and veggies',
-            price:  22.99
-        },
-        {
-            id: 'm2',
-            name: 'BBQ Burger',
-            description: 'American, raw, meaty',
-            price: 12.99
-        },
-        {
-            id: 'm3',
-            name: 'Schnitzel',
-            description: 'A German specialty!',
-            price: 16.50
-        },
-        {
-            id: 'm4',
-            name: 'Green Bowl',
-            description: 'Healthy... and green...',
-            price: 18.99
-        }
-    ]
+    const [meals, setMeals] = useState([]);
+
+    const { isLoading, error, sendRequest: fetchMeals } = useHttp();
 
     const mealsTitle = 'Delicious Food, Delivered To You';
     const summary1 = 'Choose your favorite meal from our broad selection of available meals ' +
                      'and enjoy a delicious lunch or dinner at home.';
     const summary2 = 'All our meals are cooked with high-quality ingredients, just-in-time and ' +
                      'of course by experienced chefs!'
+
+    useEffect(() => {
+        const transformMeals = (mealsObj) => {
+            const loadedMeals = [];
+
+            for (const mealKey in mealsObj) {
+                loadedMeals.push({ 
+                    id: mealKey, 
+                    name: mealsObj[mealKey].name, 
+                    description: mealsObj[mealKey].description, 
+                    price: mealsObj[mealKey].price
+                });
+            }
+
+            setMeals(loadedMeals);
+        };
+
+        fetchMeals(
+            { url: 'https://react-meals-b01d6-default-rtdb.firebaseio.com/meals.json'},
+            transformMeals
+        );
+    }, [fetchMeals]);
+
+    const displayMeals = meals.map((item) => {
+        return (
+            <MealItem
+                key={item.id}
+                meal={item}
+            />
+        )
+    });
 
     return (
         <>
@@ -48,14 +57,9 @@ const Meals = props => {
             />
             <Card className={styles.meals}>
                 <ul>
-                    {menu.map((item) => {
-                        return (
-                            <MealItem
-                                key={item.id}
-                                meal={item}
-                            />
-                        )
-                    })}
+                    {error && <p>{error}</p>}
+                    {isLoading && !error && <p>Menu is Loading...</p>}
+                    {!isLoading && !error && displayMeals}
                 </ul>
             </Card>
         </>
